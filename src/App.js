8fd6f16e761a9131621b09f70f9cc9e0bd393d9f
David Liu <david.liu@yahooinc.com>
2024-01-20 19:16:25 -0600

    just plain helloworld

--- src/App.js
+++ src/App.js
@@ -3,20 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="body">
         <p>
           Hello World!
         </p>
-        <p>
-          I will make projects here. AGAIN!
-        </p>
-        <p>
-          I will make projects here. AGAIN!
-        </p>
-        <p>
-          I will make projects here. AGAIN!
-        </p>
-      </header>
+      </div>
     </div>
   );
 }
