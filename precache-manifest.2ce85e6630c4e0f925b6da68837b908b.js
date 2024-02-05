38906f5baafff89cd45c00630e37e40386aafea6
liu00david <46476073+liu00david@users.noreply.github.com>
2024-02-05 04:43:54 +0000

    Deploying to gh-pages from @ liu00david/fun@a34df23148797799430591c8693fd8867f74a113 🚀

---
+++ precache-manifest.2ce85e6630c4e0f925b6da68837b908b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42d66e82eb711209a915",
+    "url": "/fun/static/css/main.2f53afbb.chunk.css"
+  },
+  {
+    "revision": "42d66e82eb711209a915",
+    "url": "/fun/static/js/main.faeb2c77.chunk.js"
+  },
+  {
+    "revision": "5e134c8230f9c09ac0d3",
+    "url": "/fun/static/js/runtime~main.ee35a079.js"
+  },
+  {
+    "revision": "f8258d8c17ec93ef6bf9",
+    "url": "/fun/static/js/2.e7256941.chunk.js"
+  },
+  {
+    "revision": "5f5f178e4398121915a0a2a03f06cf09",
+    "url": "/fun/index.html"
+  }
+];
