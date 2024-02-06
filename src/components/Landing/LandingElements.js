83ada2390ce15b74f6c5138cf084e24224439a15
David Liu <david.liu@yahooinc.com>
2024-02-05 20:58:04 -0800

    use type library

--- src/components/Landing/LandingElements.js
+++ src/components/Landing/LandingElements.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 export const LandingContainer = styled.div`
   height: 100vh;
@@ -14,61 +13,5 @@ export const LandingContainer = styled.div`
   justify-content: center;
   color: #fff;
   text-align: center;
+  font-size: 2em;
 `;
-
-// TypingComponent.js
-const typingAnimation = keyframes`
-  from { width: 0; }
-  to { width: 100%; }
-`;
-
-const blinkAnimation = keyframes`
-  0%, 100% { opacity: 0; }
-  50% { opacity: 1; }
-`;
-
-const TypingContainer = styled.div`
-  overflow: hidden;
-  white-space: nowrap;
-  margin: auto;
-  display: inline-block;
-  animation: ${typingAnimation} 1s steps(40, end);
-`;
-
-const BlinkingUnderscore = styled.span`
-  margin-left: 0px; /* Adjust spacing between the typed text and underscore */
-  animation: ${blinkAnimation} 1s infinite 3s; /* Add a 3s delay before starting blinking */
-`;
-
-export const TypingComponent = ({ children }) => {
-  const [text, setText] = useState('');
-
-  useEffect(() => {
-    const words = children || ''; // Use the provided text or an empty string
-    let currentIndex = -5;
-
-    const typingInterval = setInterval(() => {
-      setText(words.substring(1, currentIndex));
-      currentIndex++;
-
-      if (currentIndex > words.length) {
-        clearInterval(typingInterval);
-      }
-    }, 200); // Adjust typing speed as needed
-
-    const timeoutId = setTimeout(() => {
-      setText('');
-    }, 5000);
-
-    return () => clearTimeout(timeoutId);
-  }, [children]);
-
-  return (
-    <div>
-      <TypingContainer>
-        {text}
-      </TypingContainer>
-      <BlinkingUnderscore>_</BlinkingUnderscore>
-    </div>
-  );
-};
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,11 +1,48 @@
-import React from "react";
-import { LandingContainer, TypingComponent } from './LandingElements'
+import React, { useEffect} from 'react';
+import { LandingContainer } from './LandingElements'
+import { TypeAnimation } from 'react-type-animation';
 
 const Landing = () => {
+  const animate_1_len = 1500
+  const animate_2_len = 1500
+  const animate_3_len = 1800
+  const wait_first_page = 4500
+  const animate_4_len = 10000
+  const scroll_len = animate_1_len + animate_2_len + animate_3_len + wait_first_page
+
+  useEffect(() => {
+    const scrollDown = () => {
+      window.scrollTo({
+        top: window.scrollY + window.innerHeight,
+        behavior: 'smooth', // smooth scroll
+      });
+    };
+
+    const timeout = setTimeout(scrollDown, scroll_len);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <LandingContainer>
       <div>
-        <h1> <TypingComponent> Hello world! </TypingComponent></h1>
+        <TypeAnimation
+        sequence={[
+          // Same substring at the start will only be typed out once, initially
+          '...',
+          animate_1_len, // wait 1s before replacing "Mice" with "Hamsters"
+          'hey',
+          animate_2_len,
+          'it\'s david',
+          animate_3_len,
+          'it\'s david :)',
+          animate_4_len,
+          'it\'s david :D',
+        ]}
+        wrapper="span"
+        speed={1}
+        repeat={0}
+      />
       </div>
     </LandingContainer>
   );
